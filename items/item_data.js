acec72d15e8c5d541dc4ea96c0faa8045caf4f4c
buhalrehberlik16-web <buhalrehberlik16@gmail.com>
2026-01-03 20:05:25 +0300

    Add files via upload

--- items/item_data.js
+++ items/item_data.js
@@ -14,31 +14,31 @@ window.ITEM_CONFIG = {
 
 window.BASE_ITEMS = {
     ring: {
-        str: { nameKey: "item_ring_str", icon: "ring_str.png" },
-        dex: { nameKey: "item_ring_dex", icon: "ring_dex.png" },
-        int: { nameKey: "item_ring_int", icon: "ring_int.png" },
-        vit: { nameKey: "item_ring_vit", icon: "ring_vit.png" },
-        mp_pow: { nameKey: "item_ring_mp", icon: "ring_mp.png" }
+        str: { nameKey: "item_ring_str", icon: "ring_str.webp" },
+        dex: { nameKey: "item_ring_dex", icon: "ring_dex.webp" },
+        int: { nameKey: "item_ring_int", icon: "ring_int.webp" },
+        vit: { nameKey: "item_ring_vit", icon: "ring_vit.webp" },
+        mp_pow: { nameKey: "item_ring_mp", icon: "ring_mp.webp" }
     },
     necklace: {
-        str: { nameKey: "item_neck_str", icon: "neck_str.png" },
-        dex: { nameKey: "item_neck_dex", icon: "neck_dex.png" },
-        int: { nameKey: "item_neck_int", icon: "neck_int.png" },
-        vit: { nameKey: "item_neck_vit", icon: "neck_vit.png" },
-        mp_pow: { nameKey: "item_neck_mp", icon: "neck_mp.png" }
+        str: { nameKey: "item_neck_str", icon: "neck_str.webp" },
+        dex: { nameKey: "item_neck_dex", icon: "neck_dex.webp" },
+        int: { nameKey: "item_neck_int", icon: "neck_int.webp" },
+        vit: { nameKey: "item_neck_vit", icon: "neck_vit.webp" },
+        mp_pow: { nameKey: "item_neck_mp", icon: "neck_mp.webp" }
     },
     earring: {
-        str: { nameKey: "item_ear_str", icon: "ear_str.png" },
-        dex: { nameKey: "item_ear_dex", icon: "ear_dex.png" },
-        int: { nameKey: "item_ear_int", icon: "ear_int.png" },
-        vit: { nameKey: "item_ear_vit", icon: "ear_vit.png" },
-        mp_pow: { nameKey: "item_ear_mp", icon: "ear_mp.png" }
+        str: { nameKey: "item_ear_str", icon: "ear_str.webp" },
+        dex: { nameKey: "item_ear_dex", icon: "ear_dex.webp" },
+        int: { nameKey: "item_ear_int", icon: "ear_int.webp" },
+        vit: { nameKey: "item_ear_vit", icon: "ear_vit.webp" },
+        mp_pow: { nameKey: "item_ear_mp", icon: "ear_mp.webp" }
     },
     belt: {
-        str: { nameKey: "item_belt_str", icon: "belt_str.png" },
-        dex: { nameKey: "item_belt_dex", icon: "belt_dex.png" },
-        int: { nameKey: "item_belt_int", icon: "belt_int.png" },
-        vit: { nameKey: "item_belt_vit", icon: "belt_vit.png" },
-        mp_pow: { nameKey: "item_belt_mp", icon: "belt_mp.png" }
+        str: { nameKey: "item_belt_str", icon: "belt_str.webp" },
+        dex: { nameKey: "item_belt_dex", icon: "belt_dex.webp" },
+        int: { nameKey: "item_belt_int", icon: "belt_int.webp" },
+        vit: { nameKey: "item_belt_vit", icon: "belt_vit.webp" },
+        mp_pow: { nameKey: "item_belt_mp", icon: "belt_mp.webp" }
     }
 };
