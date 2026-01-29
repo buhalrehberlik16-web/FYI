0bdb5455c8b4459e652973462b732c292dbc1340
cialoh <47675990+cialoh@users.noreply.github.com>
2026-01-29 13:05:57 +0300

    Update item_generator.js

--- items/item_generator.js
+++ items/item_generator.js
@@ -20,7 +20,7 @@ window.generateRandomItem = function(tier) {
     // Stat puanı dağıtımı (Mevcut mantığın aynı kalıyor)
     newItem.stats[mainStat] = window.ITEM_CONFIG.multipliers.stats;
 
-    let remainingPoints = tier - 1;
+    let remainingPoints = Math.max (1, Math.floor (tier*2)-1.5)-1;
 
     while (remainingPoints > 0) {
         const shouldAddNew = Math.random() < 0.4 && newItem.propertyKeys.length < 3;
@@ -49,4 +49,5 @@ window.generateRandomItem = function(tier) {
 
     // ARTIK BURADA 'rules' tanımlamaya gerek yok, sistem subtype'dan bulacak.
     return newItem;
+
 };
