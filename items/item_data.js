88755755e45a6396483f0b1abd3f10b20f33a473
buhalrehberlik16-web <buhalrehberlik16@gmail.com>
2026-02-05 09:01:47 +0300

    Add files via upload

--- items/item_data.js
+++ items/item_data.js
@@ -37,7 +37,7 @@ window.ITEM_CONFIG = {
     },
     // Havuzlar
     statsPool: ['str', 'dex', 'int', 'vit', 'mp_pow'],
-    resistsPool: ['physical', 'fire', 'cold', 'lightning', 'poison', 'curse']
+    resistsPool: ['fire', 'cold', 'lightning', 'poison', 'curse']
 };
 
 window.BASE_ITEMS = {
@@ -119,7 +119,6 @@ window.SPECIAL_MERCH_ITEMS = [
     { id: "lizard_cold", nameKey: "merch_lizard_cold", icon: "merch_items/lizard_cold.webp", type: "passive_charm", resistType: "cold", tier: 1, stats: {}, subtype: "charm", },
     { id: "lizard_fire", nameKey: "merch_lizard_fire", icon: "merch_items/lizard_fire.webp", type: "passive_charm", resistType: "fire", tier: 1, stats: {}, subtype: "charm", },
     { id: "lizard_lightning", nameKey: "merch_lizard_lightning", icon: "merch_items/lizard_lightning.webp", type: "passive_charm", resistType: "lightning", tier: 1, stats: {}, subtype: "charm", },
-    { id: "lizard_physical", nameKey: "merch_lizard_physical", icon: "merch_items/lizard_physical.webp", type: "passive_charm", resistType: "physical", tier: 1, stats: {}, subtype: "charm", },
     { id: "lizard_curse", nameKey: "merch_lizard_curse", icon: "merch_items/lizard_curse.webp", type: "passive_charm", resistType: "curse", tier: 1, stats: {}, subtype: "charm", },
     { id: "lizard_poison", nameKey: "merch_lizard_poison", icon: "merch_items/lizard_poison.webp", type: "passive_charm", resistType: "poison", tier: 1, stats: {}, subtype: "charm", },
 
@@ -137,7 +136,6 @@ window.SPECIAL_MERCH_ITEMS = [
     { id: "scroll_mp", nameKey: "item_scroll_mp", icon: "merch_items/scroll_mp.webp", type: "stat_scroll", target: "mp_pow", price: 30, tier: 1, stats: {}, subtype: "scroll", },
 	
 	 // --- RESIST STONES (DİRENÇ TAŞLARI) ---
-    { id: "stone_phys", nameKey: "item_stone_phys", icon: "merch_items/physical_resist_stone.webp", type: "resist_stone", target: "physical", price: 40, tier: 1, stats: {}, subtype: "resist_stone", isStack: true, },
     { id: "stone_fire", nameKey: "item_stone_fire", icon: "merch_items/fire_resist_stone.webp", type: "resist_stone", target: "fire", price: 40, tier: 1, stats: {}, subtype: "resist_stone", isStack: true, },
     { id: "stone_cold", nameKey: "item_stone_cold", icon: "merch_items/cold_resist_stone.webp", type: "resist_stone", target: "cold", price: 40, tier: 1, stats: {}, subtype: "resist_stone", isStack: true, },
     { id: "stone_lightning", nameKey: "item_stone_lightning", icon: "merch_items/lightning_resist_stone.webp", type: "resist_stone", target: "lightning", price: 40, tier: 1, stats: {}, subtype: "resist_stone", isStack: true, },
