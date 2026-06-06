01f4a630e295d092419bb39b22638f71261f0b51
cialoh <47675990+cialoh@users.noreply.github.com>
2026-06-06 18:59:27 +0300

    Update game_data.js

--- game_data.js
+++ game_data.js
@@ -41,12 +41,12 @@ const CLASS_CONFIG = {
     "Magus": {
         startingStats: { str: 2, dex: 2, int: 4, vit: 3, mp_pow: 6 },
         startingResistances: { fire: 0, cold: 0, lightning: 0, curse: 0, poison: 0 },
-        startingElementalDamage: { fire: 1, cold: 1, lightning: 1, curse: 0, poison: 0 },
+        startingElementalDamage: { fire: 0, cold: 0, lightning: 0, curse: 0, poison: 0 },
         
         baseHp: 23,
 		baseResource: 80, // Başlangıç Mana sınırı
 		onHitRageGain: 0,
-		baseAttack: 9,  
+		baseAttack: 11,  
         baseDefense: 0,
 		resourceName: "mana",
         resourceColor: "#3498db",
@@ -56,7 +56,7 @@ const CLASS_CONFIG = {
         scaling: {
             hp: { stat: "vit", mult: 4 },       // Magus VIT'ten daha çok can alır
             resource: { stat: "int", mult: 5 },  // 1 INT = 5 Max Mana
-            atk: { stat: "str", mult: 0.5 },     // Magus atağını Str'den alır!
+            atk: { stat: "str", mult: 0.2 },     // Magus atağını Str'den alır!
             def: { stat: "dex", mult: 0.5 },     // Magus zırhı daha zayıf ölçeklenir
             block: { stat: "dex", mult: 0.8 },    // Magus zekasıyla blok yapar
             regen: { stat: "mp_pow", mult: 0.34 }  // Magus MP'den tam verim alır
